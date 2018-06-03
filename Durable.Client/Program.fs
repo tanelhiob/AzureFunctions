1d29c96097a7d66ae7507757688a0266f49d3211
Tanel Hiob <tanelhiob@outlook.com>
2018-06-03 23:34:43 +0300

    Initial software

---
+++ Durable.Client/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open FSharp.Data
+open System.Threading
+
+type StartFavouritePrice = JsonProvider<"JsonExamples/FavouritePricesStart.json">
+type StatusFavouritePrice = JsonProvider<"JsonExamples/FavouritePricesStatus.json">
+type FavouritePrices = JsonProvider<"JsonExamples/FavouritePrices.json">
+
+let getStatus url =
+    Http.RequestString(url) |> StatusFavouritePrice.Parse
+
+let rec pollGetStatus url =
+    printfn "getting status..."
+    let statusFavouritePrice = Http.RequestString(url) |> StatusFavouritePrice.Parse
+    printfn "status %s" statusFavouritePrice.RuntimeStatus
+
+    if statusFavouritePrice.RuntimeStatus = "Completed" then
+        statusFavouritePrice.Output
+    else
+        Thread.Sleep(3000)
+        pollGetStatus url
+
+
+[<EntryPoint>]
+let main argv =
+    let url = "http://localhost:7071/api/FavouritePlaces"
+    let startFavouritePrice = Http.RequestString(url) |> StartFavouritePrice.Parse
+    
+    let favouritePrices = pollGetStatus startFavouritePrice.StatusQueryGetUri |> FavouritePrices.Parse
+    favouritePrices |> Array.iter (fun favouritePrice -> printfn "%s (%s) %f€ %f%%" favouritePrice.Data.Name favouritePrice.Data.Symbol favouritePrice.Data.Quotes.Eur.Price favouritePrice.Data.Quotes.Eur.PercentChange24h)
+
+    0 // return an integer exit code
---
+++ Durable/Http.fs
@@ -0,0 +1,91 @@
+﻿module Http
+
+open Microsoft.Azure.WebJobs
+open System.Net.Http
+open System.Text
+open System.Net
+open System.Threading.Tasks
+open System
+open FSharp.Control.Tasks
+open Microsoft.Azure.WebJobs.Host
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+
+let getAsyncTask (url:string) = task {
+    use httpClient = new HttpClient()
+    use! response = httpClient.GetAsync(url)
+    response.EnsureSuccessStatusCode() |> ignore
+    return! response.Content.ReadAsStringAsync()
+}
+
+let getAsync = getAsyncTask >> Async.AwaitTask
+
+let createResponse (contentType:string) (content:string) : HttpResponseMessage =
+    let response = new HttpResponseMessage(HttpStatusCode.OK)
+    response.Content <- new StringContent(content, Encoding.UTF8, contentType)
+    response
+
+let createJsonResponse = createResponse "application/json"
+
+[<Literal>]
+let functionNameHello = "Hello"
+[<FunctionName(functionNameHello)>]
+let hello ([<HttpTrigger>] req: HttpRequestMessage) =
+    createResponse "application/text" "Hello World"
+
+[<Literal>]
+let functionNameGetInfo = "getInfo"
+[<FunctionName(functionNameGetInfo)>]
+let getInfo ([<ActivityTrigger>] id: int, log: TraceWriter) = task {
+    let url = "https://api.coinmarketcap.com/v2/ticker/" + (string id) + "/?convert=EUR"
+    use httpClient = new HttpClient()
+    use! response = httpClient.GetAsync(url)
+    response.EnsureSuccessStatusCode() |> ignore
+    return! response.Content.ReadAsStringAsync()
+}
+
+[<Literal>]
+let functionNameList = "list"
+[<FunctionName(functionNameList)>]
+let list ([<HttpTrigger>] req: HttpRequestMessage) =
+    getAsync("https://api.coinmarketcap.com/v2/listings")
+    |> Async.RunSynchronously
+    |> createJsonResponse
+
+(*
+    BTC 1
+    LTC 2
+    XRP 52
+    MIOTA 1720
+    NANO 1567
+    XMR 328
+    ETH 1027
+*)
+[<Literal>]
+let functionNameGetFavouritePrices = "getFavouritePrices"
+[<FunctionName(functionNameGetFavouritePrices)>]
+let getFavouritePrices ([<OrchestrationTrigger>] context: DurableOrchestrationContext) = task {
+    let favouriteCurrencyIds = [1;2;52;328;1027;1567;1720]
+    let createCryptoPriceTask id = context.CallActivityAsync<string>(functionNameGetInfo, id)
+    let tasks = favouriteCurrencyIds |> List.map createCryptoPriceTask
+    let! results = Task.WhenAll(tasks)
+
+    let jsonArray =
+        results
+        |> Array.map JObject.Parse 
+        |> Array.fold (fun (state:JArray) (element:JObject) -> state.Add(element); state) (new JArray())
+
+    return jsonArray.ToString()
+}
+
+[<Literal>]
+let functionNameFavouritePlaces = "FavouritePlaces"
+[<FunctionName(functionNameFavouritePlaces)>]
+let favouritePlaces ([<HttpTrigger>] req: HttpRequestMessage, [<OrchestrationClient>] client: DurableOrchestrationClient, log: TraceWriter) =
+    async {
+        let! instanceId = client.StartNewAsync(functionNameGetFavouritePrices, null) |> Async.AwaitTask
+        return client.CreateCheckStatusResponse(req, instanceId)
+    } |> Async.StartAsTask
+
+
+
